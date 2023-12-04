f2fa0380f904b66763bb65f0292ba311538970b1
MarijaBute <mbuteska@gmail.com>
2023-12-04 16:12:25 +0100

    Added scroll animation and finishing touches

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {useEffect}from "react";
 import './footer.css'
 import video2 from '../../Assets/video2.mp4'
 import {FiSend} from 'react-icons/fi'
@@ -7,7 +7,16 @@ import {AiOutlineTwitter, AiFillYoutube, AiFillInstagram} from 'react-icons/ai'
 import {FaTripadvisor} from 'react-icons/fa'
 import {FiChevronRight} from 'react-icons/fi'
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+
 const Footer = () => {
+
+    useEffect(()=> {
+        Aos.init({duration: 2000})
+    }, [])
+
     return (
         <section className="footer">
             <div className="videoDiv">
@@ -16,14 +25,14 @@ const Footer = () => {
 
             <div className="secContent container">
                 <div className="contactDiv flex">
-                    <div className="text">
+                    <div data-aos="fade-up" className="text">
                         <small>KEEP IN TOUCH</small>
                         <h2>Travel with us</h2>
                     </div>
 
                     <div className="inputDiv flex">
-                        <input type="text" placeholder="Enter email address:" />
-                        <button className="btn flex" type="submit">
+                        <input data-aos="fade-up" type="text" placeholder="Enter email address:" />
+                        <button data-aos="fade-up" className="btn flex" type="submit">
                            SEND <FiSend className="icon" />
                         </button>
                     </div>
@@ -37,11 +46,11 @@ const Footer = () => {
                         </a>
                     </div>
 
-                    <div className="footerParagraph">
+                    <div data-aos="fade-up" className="footerParagraph">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium labore iusto vero quam, consectetur consequuntur deserunt, odio laborum esse blanditiis repellat aliquam, ut tempore non doloribus architecto cum modi. Dolores.
                     </div>
 
-                    <div className="footerSocials flex">
+                    <div data-aos="fade-up" className="footerSocials flex">
                         <AiOutlineTwitter className="icon" />
                         <AiFillYoutube className="icon" />
                         <AiFillInstagram className="icon" />
@@ -51,7 +60,7 @@ const Footer = () => {
                
                    <div className="footerLinks grid">
                      {/* Group One*/}
-                    <div className="linkGroup">
+                    <div data-aos="fade-up" data-aos-duration="3000" className="linkGroup">
                         <span className="groupTitle">
                             OUR AGENCY
                         </span>
@@ -82,7 +91,7 @@ const Footer = () => {
                         </li>
                     </div>
                     {/* Group two*/}
-                    <div className="linkGroup">
+                    <div data-aos="fade-up" data-aos-duration="4000" className="linkGroup">
                         <span className="groupTitle">
                             PARTNERS
                         </span>
@@ -114,7 +123,7 @@ const Footer = () => {
                     </div>
 
                      {/* Group Three*/}
-                     <div className="linkGroup">
+                     <div data-aos="fade-up" data-aos-duration="5000" className="linkGroup">
                         <span className="groupTitle">
                             LAST MINUTE
                         </span>
